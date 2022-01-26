8ca94d334768ee5e100d816c105fc5e28f601162
babakdorani <babak.dorani@gmail.com>
2022-01-26 19:54:11 +0200

    routing table and routing table added

---
+++ routing-tables.tf
@@ -0,0 +1,43 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+
+  tags = {
+    Name      = "public"
+    Terraform = "true"
+  }
+}
+
+resource "aws_route_table" "private1" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.gw1.id
+  }
+
+  tags = {
+    Name = "private1"
+  }
+}
+
+
+resource "aws_route_table" "private2" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.gw2.id
+  }
+
+  tags = {
+    Name = "private2"
+  }
+}
+
+
