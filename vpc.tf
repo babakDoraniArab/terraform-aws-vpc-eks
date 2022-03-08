088359047a47b4d2a33bd22507997f63f06fb785
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2022-03-08 18:43:31 +0000

    update vpc file

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,7 @@
+################################
+## VPC
+################################
+
 resource "aws_vpc" "main" {
   cidr_block       = "192.168.0.0/16"
   instance_tenancy = "default"
@@ -9,6 +13,223 @@ resource "aws_vpc" "main" {
   assign_generated_ipv6_cidr_block = false
   tags = {
     Name      = "main"
+    Terraform = "true"
+  }
+}
+
+################################
+## Private Subnet
+################################
+
+resource "aws_subnet" "private_1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "192.168.128.0/18"
+  #AZ availability zone
+  availability_zone = "eu-west-1a"
+
+
+  tags = {
+    Name                              = "private-eu-west-1a"
+    "kubernetes.io/cluster/eks"       = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+    Terraform                         = "true"
+  }
+}
+
+
+resource "aws_subnet" "private_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "192.168.192.0/18"
+  #AZ availability zone
+  availability_zone = "eu-west-1b"
+
+
+  tags = {
+    Name                              = "private-eu-west-1b"
+    "kubernetes.io/cluster/eks"       = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+    Terraform                         = "true"
+  }
+}
+
+################################
+## Private routing table
+################################
+
+resource "aws_route_table" "private1" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.gw1.id
+  }
+
+  tags = {
+    Name = "private1"
+  }
+}
+
+resource "aws_route_table" "private2" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.gw2.id
+  }
+
+  tags = {
+    Name = "private2"
+  }
+}
+
+################################
+## Private routing table association
+################################
+
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private1.id
+}
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private2.id
+}
+
+################################
+## Public Subnet
+################################
+
+resource "aws_subnet" "public_1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "192.168.0.0/18"
+  #AZ availability zone
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                        = "public-eu-west-1a"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
+    Terraform                   = "true"
+  }
+}
+
+
+resource "aws_subnet" "public_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "192.168.64.0/18"
+  #AZ availability zone
+  availability_zone       = "eu-west-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                        = "public-eu-west-1b"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
+    Terraform                   = "true"
+  }
+}
+
+################################
+## Public routing table
+################################
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+
+  tags = {
+    Name      = "public"
+    Terraform = "true"
+  }
+}
+
+################################
+## Public routing table association
+################################
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
+################################
+## NTGW 1
+################################
+
+resource "aws_nat_gateway" "gw1" {
+  allocation_id = aws_eip.nat1.id
+  subnet_id     = aws_subnet.public_1.id
+
+  tags = {
+    Name = "gw1 NAT1"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.main]
+}
+
+################################
+## NTGW 2
+################################
+
+resource "aws_nat_gateway" "gw2" {
+  allocation_id = aws_eip.nat2.id
+  subnet_id     = aws_subnet.public_2.id
+
+  tags = {
+    Name = "NAT2 gw2"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.main]
+}
+
+################################
+## IGW
+################################
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name      = "main"
+    Terraform = "true"
+  }
+}
+
+################################
+## EIP 1
+################################
+
+resource "aws_eip" "nat1" {
+  #for availablity zone 1 ( public1 and private1 )
+  depends_on = [aws_internet_gateway.main]
+
+  tags = {
+    Terraform = "true"
+  }
+}
+
+################################
+## EIP 2
+################################
+
+resource "aws_eip" "nat2" {
+  #for availablity zone 2 ( public2 and private2 )
+  depends_on = [aws_internet_gateway.main]
+
+  tags = {
     Terraform = "true"
   }
 }
